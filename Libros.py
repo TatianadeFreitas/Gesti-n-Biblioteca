e395a7e48a72f852e54c5f3b1ea345707d629c8b
TatianadeFreitas <maitetorrespipo@gmail.com>
2026-05-26 12:42:00 -0300

    Función de Registrar Libros 5/26

--- Libros.py
+++ Libros.py
@@ -1,2 +1,31 @@
 #Funciones relacionadas a libros
 
+Libros = [] # lista donde se guardan todos los libros
+
+#Función de agregar libros
+def registrar_libro(libros, codigo, titulo, autor, anio, genero): #libro es un diccionario individual (es como ponerle nombre a la i)
+
+    for libro in Libros:
+        if libro["codigo"]==codigo:
+            print("Error: Ya existe un libro con el código", codigo)
+        return
+    
+    nuevo_libro = {
+        "codigo": codigo,
+        "titulo": titulo,
+        "autor": autor,
+        "anio": anio,
+        "genero": genero,
+        "estado": "DISPONIBLE"
+    }
+    Libros.append(nuevo_libro)
+    print("Libro registrado con éxito")
+
+
+'''
+PRUEBAS
+
+Ejecución 1: Registrar libro
+    registrar_libro(Libros, "L001", "El principito", "Antoine", 1943, "Novela") Funciona
+
+'''
