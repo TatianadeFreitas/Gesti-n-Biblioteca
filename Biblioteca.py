b71f9e6ba6e9ac0b57ec78e01e03925ef0cb6620
TatianadeFreitas <maitetorrespipo@gmail.com>
2026-05-27 12:20:02 -0300

    Inicio Biblioteca.py

--- Biblioteca.py
+++ Biblioteca.py
@@ -1,1 +1,20 @@
 #Archivo principal
+
+#Importo los modulos
+import sys
+from Libros import *
+
+# sys.argv[1] es la opcion que escribe el usuario en la consola
+opcion = sys.argv[1]
+
+if opcion == "registrar_libro":
+    codigo=sys.argv[2]
+    titulo=sys.argv[3]
+    autor=sys.argv[4]
+    anio=int(sys.argv[5]) #Lo convierto en un número
+    genero=sys.argv[6]
+
+    Libros.registrar_libro(codigo, titulo, autor, anio, genero)
+
+elif opcion == "listar_libros":
+    Libros.listar_libros()
